d19d129b644ae6fb4fcef864338ea4623966dcdb
Ricardo <luricarolo@ittepic.edu.mx>
2018-04-05 16:44:39 -0600

    crear referencias fb

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, AlertController } from 'ionic-angular';
+
+import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  tareas2: AngularFireList<any>;
+  tareas: Observable<any[]>;
+
+  constructor(public navCtrl: NavController, public alert: AlertController, public db: AngularFireDatabase) {
+
+    this.tareas2 = this.db.list('listaTareas');
+    this.tareas = this.tareas2.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
+  }
 
   }
 
-}
+
